e2fdcea2e652f61a22bd036b40760f941566636d
MoyDaniel01 <aragonmoy01@gmail.com>
2019-02-05 12:47:31 -0600

    Initial Setup

---
+++ Util/Debug/launchpad.d
@@ -0,0 +1,43 @@
+# FIXED
+
+launchpad.obj: ../launchpad.c
+launchpad.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdint.h
+launchpad.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/stdint.h
+launchpad.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/cdefs.h
+launchpad.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_types.h
+launchpad.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_types.h
+launchpad.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_stdint.h
+launchpad.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_stdint.h
+launchpad.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdbool.h
+launchpad.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdio.h
+launchpad.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/_ti_config.h
+launchpad.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/linkage.h
+launchpad.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdarg.h
+launchpad.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h
+launchpad.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h
+launchpad.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h
+launchpad.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h
+launchpad.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/systick.h
+launchpad.obj: ../launchpad.h
+launchpad.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h
+
+../launchpad.c: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/cdefs.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdbool.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdio.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/_ti_config.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/linkage.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdarg.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/systick.h: 
+../launchpad.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h: 
