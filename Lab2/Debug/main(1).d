61b7264536b6f01b54dc075d343a22a2610c4266
mreyes43 <47364561+mreyes43@users.noreply.github.com>
2019-02-12 13:31:47 -0600

    Revert "git stash"
    This reverts commit 635dcff01eaa96c481edfb0d11cb6ec288c88838.

--- Lab2/Debug/main(1).d
+++ Lab2/Debug/main(1).d
@@ -1,18 +1,18 @@
 # FIXED
 
 main(1).obj: ../main(1).c
-main(1).obj: C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdint.h
-main(1).obj: C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/stdint.h
-main(1).obj: C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/cdefs.h
-main(1).obj: C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/_types.h
-main(1).obj: C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/machine/_types.h
-main(1).obj: C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/machine/_stdint.h
-main(1).obj: C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/_stdint.h
-main(1).obj: C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdbool.h
-main(1).obj: C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdio.h
-main(1).obj: C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/_ti_config.h
-main(1).obj: C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/linkage.h
-main(1).obj: C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdarg.h
+main(1).obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdint.h
+main(1).obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/stdint.h
+main(1).obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/cdefs.h
+main(1).obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_types.h
+main(1).obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_types.h
+main(1).obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_stdint.h
+main(1).obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_stdint.h
+main(1).obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdbool.h
+main(1).obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdio.h
+main(1).obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/_ti_config.h
+main(1).obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/linkage.h
+main(1).obj: C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdarg.h
 main(1).obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h
 main(1).obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h
 main(1).obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h
@@ -20,22 +20,22 @@ main(1).obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_i2c.h
 main(1).obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h
 main(1).obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h
 main(1).obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/i2c.h
-main(1).obj: C:/Users/mreyes43/Documents/GitHub/LabGroupC2/Util/launchpad.h
+main(1).obj: C:/Users/arago/OneDrive/Documents/GitHub/LabGroupC2/Util/launchpad.h
 main(1).obj: ../seg7.h
 
 ../main(1).c: 
-C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdint.h: 
-C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/stdint.h: 
-C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/cdefs.h: 
-C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/_types.h: 
-C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/machine/_types.h: 
-C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/machine/_stdint.h: 
-C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/sys/_stdint.h: 
-C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdbool.h: 
-C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdio.h: 
-C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/_ti_config.h: 
-C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/linkage.h: 
-C:/ti/ccs810/ccsv8/tools/compiler/ti-cgt-arm_18.1.2.LTS/include/stdarg.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/cdefs.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_types.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/machine/_stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/sys/_stdint.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdbool.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdio.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/_ti_config.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/linkage.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-arm_18.1.4.LTS/include/stdarg.h: 
 C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h: 
 C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h: 
 C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h: 
@@ -43,5 +43,5 @@ C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_i2c.h:
 C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h: 
 C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h: 
 C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/i2c.h: 
-C:/Users/mreyes43/Documents/GitHub/LabGroupC2/Util/launchpad.h: 
+C:/Users/arago/OneDrive/Documents/GitHub/LabGroupC2/Util/launchpad.h: 
 ../seg7.h: 
