bd988f6236f20057ba35533cf21173764cf4f9ca
MoyDaniel01 <aragonmoy01@gmail.com>
2019-02-19 13:00:04 -0600

    update

--- Lab3/sw-increment.asm
+++ Lab3/sw-increment.asm
@@ -6,6 +6,8 @@
 ; This function increment the number shown on the stop watch display. It should be called once every one centisecond.
 					.text
 					.global stopWatchIncrement	; make this symbol global
+					.global stopWatchReset
+
 stopWatchIncrement	.asmfunc
 
 					; Update c2, continue to c1 if c2 is reset to zero
@@ -17,9 +19,44 @@ stopWatchIncrement	.asmfunc
 					MOV		r1, #0				; reset c2 to zero
 					STRB		r1, [r0, #0]			; store back again
 
-					; YOUR CODE: update c1, s2, and s1
+					LDRB		r2, [r0, #1]			; load c2 to r1, note c2's offset is zero which can be omitted
+					ADD		r2, #1				; increment c2 by one
+					STRB		r2, [r0, #1]			; store back c2
+					CMP		r2, #6				; compare c2 and 10
+					BNE		return				; if c2 != 10, jump to return
+					MOV		r2, #0				; reset c2 to zero
+					STRB		r2, [r0, #1]			; store back again
+
+					LDRB		r3, [r0, #2]			; load c2 to r1, note c2's offset is zero which can be omitted
+					ADD		r3, #1				; increment c2 by one
+					STRB		r3, [r0, #2]			; store back c2
+					CMP		r3, #10				; compare c2 and 10
+					BNE		return				; if c2 != 10, jump to return
+					MOV		r3, #0				; reset c2 to zero
+					STRB		r3, [r0, #2]			; store back again
+
+					LDRB		r4, [r0, #3]			; load c2 to r1, note c2's offset is zero which can be omitted
+					ADD		r4, #1				; increment c2 by one
+					STRB		r4, [r0, #3]			; store back c2
+					CMP		r4, #6				; compare c2 and 10
+					BNE		return				; if c2 != 10, jump to return
+					MOV		r4, #0				; reset c2 to zero
+					STRB		r4, [r0, #3]			; store back again
+
 
 return				BX		LR					; return
 					.endasmfunc
 
+stopWatchReset .asmfunc
+
+					MOV		r1, #0
+					STRB	r1, [r0, #0]
+					MOV		r2, #0
+					STRB	r2, [r0, #1]
+					MOV		r3, #0
+					STRB	r3, [r0, #2]
+					MOV		r4, #0
+					STRB	r4, [r0, #3]
+
+.endasmfunc
 					.end
